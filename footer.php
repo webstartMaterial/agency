7dd16ccc6654273d6fdbc68d01aa3d1a98c625e9
“WebstartMaterial” <web.start.contact@gmail.com>
2025-03-29 23:48:59 +0100

    maj logo

--- footer.php
+++ footer.php
@@ -94,12 +94,14 @@
   <div class="bg-gray-100 dark:bg-gray-800 transition-colors duration-300">
     <div class="container px-5 py-6 mx-auto flex items-center sm:flex-row flex-col">
       <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900 dark:text-white">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
           stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full"
           viewBox="0 0 24 24">
           <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
         </svg>
-        <span class="ml-3 text-xl"><?= htmlspecialchars($content['footer']['bottom']['brand']) ?></span>
+        <span class="ml-3 text-xl"><?= htmlspecialchars($content['footer']['bottom']['brand']) ?></span> -->
+        <img style="width:100px" src="./imgs/logo.webp" alt="Click & Visible">
+
       </a>
       <p class="text-sm text-gray-500 dark:text-gray-400 sm:ml-6 sm:mt-0 mt-4">
         <?= htmlspecialchars($content['footer']['bottom']['copyright']) ?> —
--- nav.php
+++ nav.php
@@ -5,12 +5,13 @@
   <div class="container mx-auto flex items-center justify-between px-5 py-4">
     <!-- Logo + Marque -->
     <a class="flex title-font font-medium items-center text-white mb-4 md:mb-0" href="index.php">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"
+      <!--<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor"
            stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
            class="w-10 h-10 text-white p-2 bg-purple-500 rounded-full" viewBox="0 0 24 24">
         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-      </svg>
-      <span class="ml-3 text-xl"><?= htmlspecialchars($content['header']['brand']) ?></span>
+      </svg>-->
+      <img style="width:100px" src="./imgs/logo.webp" alt="Click & Visible">
+      <!-- <span class="ml-3 text-xl"><?= htmlspecialchars($content['header']['brand']) ?></span> -->
     </a>
 
     <!-- Burger Menu Button -->
